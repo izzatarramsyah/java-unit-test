b3b13c407964718b1eca3ec78c3eacc9a6b64982
Izzat Arramsyah <adl.izzata@xl.co.id>
2023-06-01 23:42:50 +0700

    update code

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+    
+    @RequestMapping(value = "/", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String Index() {
+        return "Welcome";
+    }
+
+}
